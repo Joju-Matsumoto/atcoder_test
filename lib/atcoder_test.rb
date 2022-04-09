d38f5f1ad9a3e31f100da964fe5a923300b5230d
jj_ <avasoldier2@gmail.com>
2022-04-09 22:18:29 +0900

    first commit

---
+++ lib/atcoder_test.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "atcoder_test/version"
+
+module AtcoderTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "atcoder_test"
+
+require "test-unit"
